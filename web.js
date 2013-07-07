6222de7a0ee84bf2aea34f736b411ccf554ef126
Richard Balson <richard.balson@gmail.com>
2013-07-07 09:15:48 +0000

    Updated

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express.createserver(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
- var buf = new Buffer(fs.readFileSync("index.html","utf-8");
-  response.send(buf.toString());
-var port = process.env.PORT || 5000;
+ var buf = new buffer(fs.readfilesync("index.html"),"utf-8");
+  response.send(buf.tostring());
+});
+var port = process.env.port || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("listening on " + port);
 });
