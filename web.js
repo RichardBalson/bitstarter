291859d7946b02b9b004d89e1b7c52e18dbbcb81
Richard Balson <richard.balson@gmail.com>
2013-07-07 08:34:30 +0000

    New aDDITON

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html');
+    var buf = new Buffer (256);
+  response.send(buf.toString('utc 8',(fs.readFileSync("index.html");
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
