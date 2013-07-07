b64d04c8fbd2b14579fbf358d78a48213702a46e
Richard Balson <richard.balson@gmail.com>
2013-07-07 08:44:10 +0000

    New

--- web.js
+++ web.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buf = new Buffer (256);
-  response.send(buf.toString('utc 8',(fs.readFileSync("index.html");
+    var fs = require('fs');
+ var buf = new Buffer (256,'utf8');
+  response.send(buf.toString('utf8',(fs.readFileSync("index.html");
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
